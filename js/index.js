9986e44812248fb37c4610a5c7a2b8ac51e00177
Bressendorff <95355589+Bressendorff@users.noreply.github.com>
2022-09-30 13:58:12 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,40 @@
+const app = Vue.createApp({
+
+    data() {
+
+        return {
+            "times": 0,
+            "msg": "",
+            "returnMessage": ""
+
+            // add here
+
+        }
+
+    },
+
+    methods: {
+
+        // add here
+        repeatMessage(){
+            this.returnMessage = "";
+            let message = this.msg;
+            if (this.times < 0){
+                message = `Must be non-negative number: ${this.times}`;
+            }
+            else if(this.times === 0){
+                message = "";
+            }
+            else{
+                for (let i = 0; i < this.times -1; i++){
+                    message = message + this.msg;
+                }
+            }
+            this.returnMessage = message;
+            
+            
+        }
+
+    }
+
+})
